e4562915beafced9ee5ceb4f30d3fac4ddef0e5f
Zubayer Hossain <zubayer94@gmail.com>
2021-12-12 09:29:09 +0600

    changed login and create account layout

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 import ImageLight from '../assets/img/create-account-office.jpeg'
 import ImageDark from '../assets/img/create-account-office-dark.jpeg'
@@ -7,6 +7,24 @@ import { GithubIcon, TwitterIcon } from '../icons'
 import { Input, Label, Button } from '@windmill/react-ui'
 
 function Login() {
+
+  const history = useHistory();
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [password_confirmation, setPassword_confirmation] = useState('')
+
+
+  const handlRegister = (e) => {
+    e.preventDefault();
+    // let userinfo = { name, email, website, password, password_confirmation }
+    // userRegister(userinfo)
+    //   .then(() => {
+    //     history.push('/');
+    //   })
+    //   .catch(err => console.log(err))
+  }
+
   return (
     <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
@@ -30,41 +48,28 @@ function Login() {
               <h1 className="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
                 Create account
               </h1>
-              <Label>
-                <span>Email</span>
-                <Input className="mt-1" type="email" placeholder="john@doe.com" />
-              </Label>
-              <Label className="mt-4">
-                <span>Password</span>
-                <Input className="mt-1" placeholder="***************" type="password" />
-              </Label>
-              <Label className="mt-4">
-                <span>Confirm password</span>
-                <Input className="mt-1" placeholder="***************" type="password" />
-              </Label>
+              <form onSubmit={handlRegister} >
+                <Label>
+                  <span>Name</span>
+                  <Input className="mt-1" onChange={(e) => {setName(e.target.value)}} value={name} type="text" placeholder="John Doe" />
+                </Label>
+                <Label>
+                  <span>Email</span>
+                  <Input className="mt-1" onChange={(e) => {setEmail(e.target.value)}} value={email} type="email" placeholder="john@doe.com" />
+                </Label>
+                <Label className="mt-4">
+                  <span>Password</span>
+                  <Input className="mt-1" onChange={(e) => {setPassword(e.target.value)}} value={password} placeholder="***************" type="password" />
+                </Label>
+                <Label className="mt-4">
+                  <span>Confirm password</span>
+                  <Input className="mt-1" onChange={(e) => { setPassword_confirmation(e.target.value)}} value={password_confirmation} placeholder="***************" type="password" />
+                </Label>
 
-              <Label className="mt-6" check>
-                <Input type="checkbox" />
-                <span className="ml-2">
-                  I agree to the <span className="underline">privacy policy</span>
-                </span>
-              </Label>
-
-              <Button tag={Link} to="/login" block className="mt-4">
-                Create account
-              </Button>
-
-              <hr className="my-8" />
-
-              <Button block layout="outline">
-                <GithubIcon className="w-4 h-4 mr-2" aria-hidden="true" />
-                Github
-              </Button>
-              <Button block className="mt-4" layout="outline">
-                <TwitterIcon className="w-4 h-4 mr-2" aria-hidden="true" />
-                Twitter
-              </Button>
-
+                <Button block className="mt-4" type='submit' >
+                  Create account
+                </Button>
+              </form>
               <p className="mt-4">
                 <Link
                   className="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
