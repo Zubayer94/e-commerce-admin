d1fb73301c0374ecb9acc0bad8e0511ee1db9c61
zubayer94 <zubayer94@gmail.com>
2021-12-12 19:15:16 +0600

    adding register AsyncThunk

--- src/Store/Slices/authSlice.js
+++ src/Store/Slices/authSlice.js
@@ -11,8 +11,19 @@ export const login = createAsyncThunk(
     'auth/login',
     async (payload) => {
         try {
-            console.log('got hit!');
             const response = await axios.post('/login', payload)
+            return response.data
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+)
+export const register = createAsyncThunk(
+    'auth/register',
+    async (payload) => {
+        try {
+            console.log('got hit!');
+            const response = await axios.post('/register', payload)
             return response.data
         } catch (error) {
             throw Error(error)
@@ -29,7 +40,7 @@ const authSlice = createSlice({
         }
     },
     extraReducers: {
-        [login.pending]: (state, action) => {
+        [login.pending]: (state) => {
             state.isloading = true
             state.error = null
         },
@@ -42,7 +53,23 @@ const authSlice = createSlice({
         [login.rejected]: (state, action) => {
             state.isloading = false 
             state.error = action.error.message
-        }
+        },
+
+        [register.pending]: (state) => {
+            state.isloading = true
+            state.error = null
+        },
+        [register.fulfilled]: (state, action) => {
+            state.isloading = false 
+            // state.authUser = action.payload
+            // localStorage.setItem('user', JSON.stringify(action.payload));
+            state.error = null
+        },
+        [register.rejected]: (state, action) => {
+            state.isloading = false 
+            state.error = action.error.message
+        },
+
     }
 })
 
